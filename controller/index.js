d2ee51f776934ab1793efeb75610f8d2004a9cba
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-10-17 14:48:13 +0600

    index updated

--- controller/index.js
+++ controller/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const geoip2 = require('geoip2');
 const mysql = require('mysql');
 
-var con = mysql.createConnection({
+var sql = mysql.createConnection({
   host: "192.168.11.20",
   user: "root",
   password: "minhaz1234",
@@ -12,16 +12,19 @@ var con = mysql.createConnection({
 });
 
 
-/* GET home page. */
+/* GET all data from db. */
 router.get('/', function(req, res, next) {
-  con.connect(function(err) {
+  sql.connect(function(err) { 
     let data=[];
+    let qry = "SELECT * FROM log LIMIT 20";
+
     if (err) throw err;
-    con.query("SELECT * FROM log LIMIT 20", function (err, result) {
+    sql.query(qry, function (err, result) {
       if (err) throw err;
       for (let key in result) {
         data.push(geoip2.lookupSync(result[key].ip));
       }
+
       console.log(data);
       res.send(data);
     });
