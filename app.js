cc5df681f672bf80b2a5ce46b2512695e61c5d3d
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-10-18 16:17:45 +0600

    app.js updated

--- app.js
+++ app.js
@@ -13,12 +13,43 @@ sql.connect();
 geoip2.init('../GeoLite2-ASN_20181009/GeoLite2-ASN.mmdb');
 
 
+// f={};
+// console.log(f);
+// a='abc';
+// f[a]=12;
+// f[a]+=12;
+// console.log(f);
+// a='bax';
+// f[a]=1;
+// f[a]+=1;
+// console.log(f);
+// b='mac1';
+// f[a][b] = 12;
+// b='mac2'
+// f[a][b] = 42;
+// console.log(f[a][b]);
+
+// function sum( obj ) {
+//   var sum = 0;
+//   for( var el in obj ) {
+//     if( obj.hasOwnProperty( el ) ) {
+//       sum += parseInt( obj[el] );
+//     }
+//   }
+//   return sum;
+// }
+// console.log(sum(f));
+// console.log(f[a]);
+// console.log(f);
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api', indexController);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
