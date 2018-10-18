da50e2def223abb5808d84b21530e2e80f05210c
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-10-18 16:17:32 +0600

    index.js updated

--- controller/index.js
+++ controller/index.js
@@ -4,6 +4,32 @@ const router = express.Router();
 const geoip2 = require('geoip2');
 const sql = require('../config/db');
 
+
+// f={};
+// console.log(f);
+// a='abc';
+// f[a]={};
+// console.log(f);
+// b='mac1';
+// f[a][b] = 12;
+// b='mac2'
+// f[a][b] = 42;
+// console.log(f[a][b]);
+
+function sum( obj ) {
+  var sum = 0;
+  for( var el in obj ) {
+    if( obj.hasOwnProperty( el ) ) {
+      sum += parseInt( obj[el] );
+    }
+  }
+  return sum;
+}
+// console.log(sum(f[a]));
+// console.log(f[a]);
+// console.log(f);
+
+
 /* GET all data from db. */
 router.get('/', function(req, res, next) {
     let data=[];
@@ -35,11 +61,11 @@ router.get('/mac', function(req, res, next) {
 
   console.log(req.query);
   
-  let qryForSummary = "SELECT distinct mac FROM log WHERE timestamp BETWEEN UNIX_TIMESTAMP(" + 
+  let qryForMac = "SELECT distinct mac FROM log WHERE timestamp BETWEEN UNIX_TIMESTAMP(" + 
   req.query.from +") and UNIX_TIMESTAMP(" + req.query.to + ")";
 
-  sql.query(qryForSummary, function (err, data) {
-    if (err) throw err;
+  sql.query(qryForMac, function (err, data) {
+    if (err) res.status(404).send("Mac Address not found");
     // console.log(data[0].mac);
     res.send(data);
   });
@@ -48,15 +74,29 @@ router.get('/mac', function(req, res, next) {
 
 /* /ip?from=A&to=B&mac=M # retuns {mac: ip} with datetime limit */
 router.get('/ip', function(req, res, next) {
+  let f = {};
+  let a, count;
+  let qryForIp = "SELECT ip From log WHERE mac=" + req.query.mac + " AND timestamp BETWEEN UNIX_TIMESTAMP(" + 
+  req.query.from +") AND UNIX_TIMESTAMP(" + req.query.to + ")";
 
-  console.log(req.query);
-  let qryForSummary = "SELECT ip From log WHERE mac=" + req.query.mac + " AND timestamp BETWEEN UNIX_TIMESTAMP(" + 
-  req.query.from +") AND UNIX_TIMESTAMP(" + req.query.to + ") LIMIT 50";
-
-  sql.query(qryForSummary, function (err, data) {
+  sql.query(qryForIp, function (err, data) {
     if (err) throw err;
-    // console.log(data);
-    res.send(data);
+    for (const key in data) {
+      // ipAdd.push(geoip2.lookupSync(data[key].ip));
+      a = geoip2.lookupSync(data[key].ip);
+      if (!f[a.autonomous_system_organization]) {
+        f[a.autonomous_system_organization] = 1;
+      } else {
+        f[a.autonomous_system_organization] += 1;
+      }     
+    }
+    count = sum(f);
+    for (const key in f) {
+      // console.log(key + ": " + (f[key]/count*100).toFixed(2) + "%");
+      f[key] = (f[key]/count*100).toFixed(2) + "%";
+    }
+    // console.log(f);
+    res.send(f);
   });
 
 });
