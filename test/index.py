2b8fcb92e73dced90f672678743dd2ef495be8ef
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-10-21 13:01:03 +0600

    test cases updated

--- test/index.py
+++ test/index.py
@@ -20,20 +20,21 @@ class TestStringMethods(unittest.TestCase):
     def test_weblog_mac(self):
         r = requests.get("http://localhost:3000/api/mac?from='2018-10-01 16:00:00'&to='2018-10-012 16:00:00'")
         data = r.json()
-        # print(data[0]['mac'])
-        self.assertRegexpMatches(data[0]['mac'], "[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$")
+        # print(data[1])
+        for ob in data:
+            try:
+                self.assertRegexpMatches(ob['mac'], "[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$")
+            except AssertionError:
+                self.assertRegexpMatches(ob['mac'], "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+            
 
+    def test_weblog_org_percent(self):
+        r = requests.get("http://localhost:3000/api/ip?from='2018-10-01 16:00:00'&to='2018-10-03 16:00:00'&mac='c4:85:08:7a:29:52'")
+        data = r.json()
+        for ob in data:
+            self.assertIsInstance(ob["name"], str)
+            self.assertIsInstance(float(ob["y"]), float)
 
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
-
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
 
 if __name__ == '__main__':
     # unittest.main()
