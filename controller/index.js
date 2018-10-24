4faf318160f1f58e5da32725bc715f8a7e7fd29a
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-10-24 18:28:09 +0600

    index.js updated

--- controller/index.js
+++ controller/index.js
@@ -43,31 +43,31 @@ router.get('/summary', function(req, res, next) {
 
 
 /* /mac# returns uniq list of mac addresses */
-router.get('/mac', function(req, res, next) {
+// router.get('/mac', function(req, res, next) {
   
-  let qryForMac = "SELECT distinct mac FROM log";
+//   let qryForMac = "SELECT distinct mac FROM log";
 
-  sql.query(qryForMac, function (err, data) {
-    if (err) res.status(400).send("Bad request");
-    // console.log(data[0].mac);
-    if (data == 0) {
-      res.status(404).send("Mac Address not found");
-    }
-    else {
-      res.send(data);
-    }
-  });
+//   sql.query(qryForMac, function (err, data) {
+//     if (err) res.status(400).send("Bad request");
+//     // console.log(data[0].mac);
+//     if (data == 0) {
+//       res.status(404).send("Mac Address not found");
+//     }
+//     else {
+//       res.send(data);
+//     }
+//   });
 
-});
+// });
 
 /* /mac?from='2018-10-01 16:00:00'&to='2018-10-07 16:00:00' # returns uniq list of mac addresses with datetime limit */
 router.get('/mac', function(req, res, next) {
 
   console.log(req.query);
   
-  let qryForMac = "SELECT distinct mac FROM log WHERE timestamp BETWEEN UNIX_TIMESTAMP(" + 
-  req.query.from +") and UNIX_TIMESTAMP(" + req.query.to + ")";
-
+  let qryForMac = "SELECT distinct mac FROM log WHERE timestamp BETWEEN " + req.query.from + " AND " + req.query.to + "";
+  console.log(qryForMac);
+  
   sql.query(qryForMac, function (err, data) {
     if (err) res.status(400).send("Bad request");
     // console.log(data[0].mac);
@@ -82,39 +82,55 @@ router.get('/mac', function(req, res, next) {
 });
 
 /* /ip?from=A&to=B&mac=M # retuns {mac: ip} with datetime limit */
-router.get('/ip', function(req, res, next) {
+router.get('/ip', function (req, res, next) {
   let f = {};
   let a, count;
-  let qryForIp = "SELECT ip From log WHERE mac=" + req.query.mac + " AND timestamp BETWEEN UNIX_TIMESTAMP(" + 
-  req.query.from +") AND UNIX_TIMESTAMP(" + req.query.to + ")";
+  console.log(req.query);
+  
+  let qryForIp = "SELECT ip, timestamp AS time From log WHERE mac=" + req.query.mac + " AND timestamp BETWEEN " + req.query.from + " AND " + req.query.to + "";
 
+  console.log(qryForIp);
+  
   sql.query(qryForIp, function (err, data) {
     if (err) res.status(400).send("Bad request");
     try {
       for (const key in data) {
-          a = geoip2.lookupSync(data[key].ip);
-          
-          if (a != null) {
+        a = geoip2.lookupSync(data[key].ip);        
+        if (a != null) {
+          if (!f[a.autonomous_system_organization]) {
+            f[a.autonomous_system_organization] = 1;
+          } else {
+            f[a.autonomous_system_organization] += 1;
+          }
+        } else{
+            a = { autonomous_system_organization: 'local or n/a'};
             if (!f[a.autonomous_system_organization]) {
               f[a.autonomous_system_organization] = 1;
             } else {
               f[a.autonomous_system_organization] += 1;
             }
-          } 
+            // console.log(a);           
+            // console.log(f[a]);           
+            // console.log(f);           
         }
+      }
+      // console.log(a);      
+      console.log(f);
       count = sum(f);
       let per = []
       for (const key in f) {
-        val = parseFloat((f[key]/count*100).toFixed(2));
-        per.push({name: key, y:val})     
+        val = parseFloat((f[key] / count * 100).toFixed(2));
+        per.push({ name: key, y: val, startTime: data[0].time, endTime: data[data.length-1].time})
       }
-      res.send(per);       
+      res.send(per);
     } catch (error) {
-      res.status(404).send(error.message);        
+      res.status(404).send(error.message);
     }
-    
   });
-
 });
 
 module.exports = router;
+
+// "SELECT ip, timestamp AS time From log WHERE mac="" AND timestamp BETWEEN '1538363117' AND '1538399117'"
+// 1538363117
+// 1538399117
