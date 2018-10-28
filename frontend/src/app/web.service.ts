d5cfa938f236c49a5a41b5a4557fed92e53e155c
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-10-28 14:22:14 +0600

    hostname changed to host

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -5,14 +5,14 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class WebService {
-  private host = window.location.hostname
+  private host = window.location.host
 
   constructor(private _http: HttpClient) { }
 
   /**
    * GET Method: Get overall status of net usages
    * 
-   * api: http://localhost:3000/api/summary
+   * api: http://localhost/api/summary
    * 
    * return Observable of status Json Object
    * 
@@ -25,7 +25,7 @@ export class WebService {
    *    }
    */
   getWebLogSummary() {
-    return this._http.get("http://" + this.host + ":3000/api/summary");
+    return this._http.get("http://" + this.host + "/api/summary");
   }
 
 
@@ -36,7 +36,7 @@ export class WebService {
    * parameter: sdate (Number: int of Unix timestamp in second), 
    *            edate (Number: int of Unix timestamp in second)
    * 
-   * api: http://localhost:3000/api/mac?from=sdate&to=edate
+   * api: http://localhost/api/mac?from=sdate&to=edate
    * 
    * return Observable of Array of Json Object
    * 
@@ -52,11 +52,11 @@ export class WebService {
    */
   getMacAddressesWithTime(sDate: Date, eDate: Date) {
     if (sDate == undefined && eDate == undefined) {
-      return this._http.get("http://" + this.host + ":3000/api/mac?from='1538360857'&to='1538922457'");
+      return this._http.get("http://" + this.host + "/api/mac?from='1538360857'&to='1538922457'");
     } else {
       let sdate = parseInt((sDate.getTime() / 1000).toString());
       let edate = parseInt((eDate.getTime() / 1000).toString());
-      return this._http.get("http://" + this.host + ":3000/api/mac?from='" + sdate + "'&to='" + edate + "'");
+      return this._http.get("http://" + this.host + "/api/mac?from='" + sdate + "'&to='" + edate + "'");
     }
   }
 
@@ -67,7 +67,7 @@ export class WebService {
    * parameter: sdate (Number: int of Unix timestamp in second), 
    *            edate (Number: int of Unix timestamp in second)
    * 
-   * api: http://localhost:3000/api/ip?from=sdate&to=edate&mac=m
+   * api: http://localhost/api/ip?from=sdate&to=edate&mac=m
    * 
    * return Observable of Array of Json Object
    * 
@@ -85,11 +85,11 @@ export class WebService {
    */
   getWebUsages(m: any, sDate?: Date, eDate?: Date) {
     if (sDate == undefined && eDate == undefined) {
-      return this._http.get("http://" + this.host + ":3000/api/ip?from='1538360857'&to='1538922457'&mac='" + m + "'");
+      return this._http.get("http://" + this.host + "/api/ip?from='1538360857'&to='1538922457'&mac='" + m + "'");
     } else {
       let sdate = parseInt((sDate.getTime() / 1000).toString());
       let edate = parseInt((eDate.getTime() / 1000).toString());
-      return this._http.get("http://" + this.host + ":3000/api/ip?from='" + sdate + "'&to='" + edate + "'&mac='" + m + "'");
+      return this._http.get("http://" + this.host + "/api/ip?from='" + sdate + "'&to='" + edate + "'&mac='" + m + "'");
     }
   }
 }
